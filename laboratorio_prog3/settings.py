5debd34c665df8fdf6a6302f8f10362e402b60e4
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-06 17:29:56 -0300

    base de datos

--- laboratorio_prog3/settings.py
+++ laboratorio_prog3/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'laboratorio_prog3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestion-trabajos-finales',
+        'USER': 'user-lab-prog3',
+        'PASSWORD': '123',
     }
 }
 
