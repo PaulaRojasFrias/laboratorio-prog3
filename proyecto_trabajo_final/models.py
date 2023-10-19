7407814f38e17055658e3db05556b10f2c0fe839
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-19 13:12:26 -0300

    modificaciones app ptf

--- proyecto_trabajo_final/models.py
+++ proyecto_trabajo_final/models.py
@@ -2,21 +2,11 @@ from django.db import models
 
 from persona.models import Alumno, Docente
 
-
 # Create your models here.
 
 
-class InformeTrabajoFinal(models.Model):
-    borrador = models.FileField()
-    fechaPresentacion = models.DateField()
-
-class EstadoProyectoFinal(models.Model):
-    estado= models.CharField(max_length=50)
-    fechaMovimiento= models.DateField()
-    archivoAsociado=  models.FileField()
-    tipoMovimiento= models.CharField(max_length=50)
-
 class ProyectoFinal(models.Model):
+    id=  models.CharField(max_length=8, unique=True)
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
     fechaPresentacion = models.DateField()
@@ -24,15 +14,26 @@ class ProyectoFinal(models.Model):
     certificadoAnalitico= models.FileField()
     notaAceptacionDirector= models.FileField()
     curriculumVitaeAsesor= models.FileField()
-    alumnos = models.ManyToManyField(Alumno, related_name='trabajos')
-    docenteDirector= models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True, related_name='trabajos_dirigidos')
-    asesor= models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True, related_name='trabajos_asesorados')
+    director= models.OneToOneField(Docente, on_delete=models.SET_NULL)
+    coDirector= models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True, blank=True)
+    asesor= models.OneToOneField(Docente, on_delete=models.SET_NULL, null=True, blank=True)
     fechaAltaDirector= models.DateField()
     fechaBajaDirector= models.DateField(null=True, blank=True)
-    informe = models.OneToOneField( InformeTrabajoFinal, on_delete=models.SET_NULL, null=True, blank=True)
-    estado= models.OneToOneField(  EstadoProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+            return f'{self.titulo}'
 
 
-  def __str__(self):
-        return self.titulo
+class Movimientos(models.Model):
+    proyectoFinal= models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
+    estado= models.CharField(max_length=50)
+    fechaMovimiento= models.DateField()
+    archivoAsociado=  models.FileField(null=True, blank=True)
+    tipoMovimiento= models.CharField(max_length=50)
+    fechaVencimiento= models.DateField(null=True, blank=True)
+    fechaCulminacion= models.DateField(null=True, blank=True)
 
+
+class PTF_Integrantes(models.Model):
+    proyectoFinal= models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
+    alumnos= models.ForeignKey(Alumno, on_delete= models.CASCADE)
