0f930228475269aa0e440d1234da066b485aca1e
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-19 18:30:34 -0300

    migraciones y cambios ptf

--- proyecto_trabajo_final/models.py
+++ proyecto_trabajo_final/models.py
@@ -6,35 +6,35 @@ from persona.models import Alumno, Docente
 
 
 class ProyectoFinal(models.Model):
-    id=  models.CharField(max_length=8, unique=True)
+    id = models.CharField(primary_key=True, max_length=8, unique=True)
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
     fechaPresentacion = models.DateField()
     proyectoFinal = models.FileField()
-    certificadoAnalitico= models.FileField()
-    notaAceptacionDirector= models.FileField()
-    curriculumVitaeAsesor= models.FileField()
-    director= models.OneToOneField(Docente, on_delete=models.SET_NULL)
-    coDirector= models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True, blank=True)
-    asesor= models.OneToOneField(Docente, on_delete=models.SET_NULL, null=True, blank=True)
-    fechaAltaDirector= models.DateField()
-    fechaBajaDirector= models.DateField(null=True, blank=True)
+    notaAceptacionDirector = models.FileField()
+    director = models.OneToOneField(Docente, on_delete=models.SET_NULL, null=True, related_name='proyectos_director')
+    coDirector = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True, blank=True, related_name='proyectos_co_director')
+    asesor= models.OneToOneField(Docente, on_delete=models.SET_NULL, null=True, blank=True, related_name='proyectos_asesor')
+    fechaAltaDirector = models.DateField()
+    fechaBajaDirector = models.DateField(null=True, blank=True)
     
     def _str_(self):
             return f'{self.titulo}'
 
 
 class Movimientos(models.Model):
-    proyectoFinal= models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
-    estado= models.CharField(max_length=50)
-    fechaMovimiento= models.DateField()
-    archivoAsociado=  models.FileField(null=True, blank=True)
-    tipoMovimiento= models.CharField(max_length=50)
-    fechaVencimiento= models.DateField(null=True, blank=True)
-    fechaCulminacion= models.DateField(null=True, blank=True)
+    proyectoFinal = models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
+    estado = models.CharField(max_length=50)
+    fechaMovimiento = models.DateField()
+    archivoAsociado = models.FileField(null=True, blank=True)
+    tipoMovimiento = models.CharField(max_length=50)
+    fechaVencimiento = models.DateField(null=True, blank=True)
+    fechaCulminacion = models.DateField(null=True, blank=True)
 
 
 class PTF_Integrantes(models.Model):
-    proyectoFinal= models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
-    alumnos= models.ForeignKey(Alumno, on_delete= models.CASCADE)
-
+    proyectoFinal = models.OneToOneField(ProyectoFinal, on_delete=models.SET_NULL, null=True, blank=True)
+    alumnos = models.ForeignKey(Alumno, on_delete= models.CASCADE)
+    fechaAltaAlumno = models.DateField(null=True, blank=True)
+    fechaBajaAlumno = models.DateField(null=True, blank=True)
+    certificadoAnalitico = models.FileField()
