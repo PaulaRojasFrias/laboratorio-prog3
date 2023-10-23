19919056813c0c0ba42354a079a895c9f8514c20
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-22 21:37:35 -0300

    primeras vistas y urls

--- laboratorio_prog3/urls.py
+++ laboratorio_prog3/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.home),
+    path('regCSTF/', views.regCSTF),
+    path('regTribunal/', views.regTribunal),
+    path('regAlumno/', views.regAlumno),
+    #path('regPTF', ),
+    #path('regMovimientoPTF', ),
+
+
 ]
+
