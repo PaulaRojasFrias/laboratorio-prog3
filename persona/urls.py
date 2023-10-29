2a7ec2cb12148308bfe47b8457840652e9978e54
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-10-29 19:06:43 -0300

    vistas persona

--- persona/urls.py
+++ persona/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
-from .views import registro_docente,  registro_alumno, registro_asesor
+from .views import registro_docente,  registro_alumno, registro_asesor, lista_docente, detalle_docente, edit_docente, delete_docente, lista_alumno, detalle_alumno, edit_alumno, delete_alumno, lista_asesor, detalle_asesor, edit_asesor, delete_asesor
 
 app_name = 'persona'
 urlpatterns = [
     path('regDocente/', registro_docente, name='registro_docente'),
+    path('listaDocente/', lista_docente, name='lista_docente'),
+    path('detalleDocente/<int:pk>/', detalle_docente, name='detalle_docente'),
+    path('editDocente/<int:pk>', edit_docente, name='edit_docente'),
+    path('deleteDocente/', delete_docente, name='delete_docente'),
+
+
     path('regAlumno/', registro_alumno, name='registro_alumno'),
+    path('listaAlumno/', lista_alumno, name='lista_alumno'),
+    path('detalleAlumno/<int:pk>/', detalle_alumno, name='detalle_alumno'),
+    path('editAlumno/<int:pk>', edit_alumno, name='edit_alumno'),
+    path('deleteAlumno/', delete_alumno, name='delete_alumno'),
+
+
     path('regAsesor/', registro_asesor, name='registro_asesor'),
+    path('listaAsesor/', lista_asesor, name='lista_asesor'),
+    path('detalleAsesor/<int:pk>/', detalle_asesor, name='detalle_asesor'),
+    path('editAsesor/<int:pk>', edit_asesor, name='edit_asesor'),
+    path('deleteAsesor/', delete_asesor, name='delete_asesor'),
+
 ]
--- persona/views.py
+++ persona/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-#from .models import Docente
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Docente, Alumno, Asesor
 from .forms import DocenteForm, AlumnoForm, AsesorForm
+from django.urls import reverse
+from django.contrib import messages
 
 # Create your views here.
+#<<<<<<<<<<<<<<<<<<<<<<< DOCENTE >>>>>>>>>>>>>>>>>>>>>>
 def registro_docente(request):
     form = DocenteForm()
 
@@ -14,6 +17,43 @@ def registro_docente(request):
     context = {'form': form}
 
     return render(request, 'persona/regDocente.html', context)
+
+
+def lista_docente(request):
+    docentes = Docente.objects.all()
+    return render(request, 'persona/listaDocente.html', {'docentes': docentes})
+
+def detalle_docente(request, pk):
+    docente = get_object_or_404(Docente, pk=pk)
+    return render(request, 'persona/detalleDocente.html', {'docente': docente})
+
+def edit_docente(request, pk):
+    docente = get_object_or_404(Docente, pk=pk)
+    if request.method == 'POST':
+        docente_form = DocenteForm(request.POST, request.FILES, instance=docente)
+        if docente_form.is_valid():
+            docente_form.save()
+            messages.success(request, 'Se ha actualizado correctamente el docente')
+            return redirect(reverse('persona:detalle_docente', args=[docente.id]))
+    else:
+        docente_form = DocenteForm(instance=docente)
+
+    return render(request, 'persona/editDocente.html', {'form': docente_form})
+
+
+def delete_docente(request):
+    if request.method == 'POST':
+        if 'id_docente' in request.POST:
+            docente = get_object_or_404(Docente, pk=request.POST['id_docente'])
+            docente.delete()
+            messages.success(request, 'Se ha eliminado existosamente el docente  {}' )
+        else:
+            messages.error(request, 'Debe indicar que Docente desea eliminar')
+    return redirect(reverse('persona:lista_docente'))
+
+
+
+#<<<<<<<<<<<<<<<<<<<<<<< ALUMNO >>>>>>>>>>>>>>>>>>>>>>
 
 def registro_alumno(request):
     form = AlumnoForm()
@@ -27,6 +67,46 @@ def registro_alumno(request):
 
     return render(request, 'persona/regAlumno.html', context)
 
+
+def lista_alumno(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'persona/listaAlumno.html', {'alumnos': alumnos})
+
+
+def detalle_alumno(request, pk):
+    alumno = get_object_or_404(Alumno, pk=pk)
+    return render(request, 'persona/detalleAlumno.html', {'alumno': alumno})
+
+
+def edit_alumno(request, pk):
+    alumno = get_object_or_404(Alumno, pk=pk)
+    if request.method == 'POST':
+        alumno_form = AlumnoForm(request.POST, request.FILES, instance=alumno)
+        if alumno_form.is_valid():
+            alumno_form.save()
+            messages.success(request, 'Se ha actualizado correctamente el alumno')
+            return redirect(reverse('persona:detalle_alumno', args=[alumno.id]))
+    else:
+        alumno_form = AlumnoForm(instance=alumno)
+
+    return render(request, 'persona/editAlumno.html', {'form': alumno_form})
+
+
+
+def delete_alumno(request):
+    if request.method == 'POST':
+        if 'id_alumno' in request.POST:
+            alumno = get_object_or_404(Alumno, pk=request.POST['id_alumno'])
+            alumno.delete()
+            messages.success(request, 'Se ha eliminado existosamente el alumno  {}')
+        else:
+            messages.error(request, 'Debe indicar que Alumno desea eliminar')
+    return redirect(reverse('persona:lista_alumno'))
+
+
+
+#<<<<<<<<<<<<<<<<<<<<<<< ASESOR >>>>>>>>>>>>>>>>>>>>>>
+
 def registro_asesor(request):
     form = AsesorForm()
 
@@ -38,3 +118,37 @@ def registro_asesor(request):
     context = {'form': form}
 
     return render(request, 'persona/regAsesor.html', context)
+
+
+def lista_asesor(request):
+    asesores = Asesor.objects.all()
+    return render(request, 'persona/listaAsesor.html', {'asesores': asesores})
+
+
+def detalle_asesor(request, pk):
+    asesor = get_object_or_404(Asesor, pk=pk)
+    return render(request, 'persona/detalleAsesor.html', {'asesor': asesor})
+
+
+def edit_asesor(request, pk):
+    asesor = get_object_or_404(Asesor, pk=pk)
+    if request.method == 'POST':
+        asesor_form = AsesorForm(request.POST, request.FILES, instance=asesor)
+        if asesor_form.is_valid():
+            asesor_form.save()
+            messages.success(request, 'Se ha actualizado correctamente el asesor')
+            return redirect(reverse('persona:detalle_asesor', args=[asesor.id]))
+    else:
+        asesor_form = DocenteForm(instance=asesor)
+
+    return render(request, 'persona/editAsesor.html', {'form': asesor_form})
+
+def delete_asesor(request):
+    if request.method == 'POST':
+        if 'id_asesor' in request.POST:
+            asesor = get_object_or_404(Asesor, pk=request.POST['id_asesor'])
+            asesor.delete()
+            messages.success(request, 'Se ha eliminado existosamente el asesor  {}' )
+        else:
+            messages.error(request, 'Debe indicar que asesor desea eliminar')
+    return redirect(reverse('persona:lista_asesor'))
